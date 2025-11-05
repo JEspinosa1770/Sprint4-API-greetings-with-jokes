42e2942a5b1d922dbd81d21faa376c7c3501da15
JEspinosa1770 <jespinosa1770@gmail.com>
2025-11-05 13:22:06 +0100

    feat/fix: perform getJoke function. Modify the name of the testing file.

---
+++ src/joke.ts
@@ -0,0 +1,23 @@
+
+import type DataJoke from "./interfaces";
+
+export async function getJoke(url: string): Promise<DataJoke | undefined> {
+    try {
+        const answer: Response = await fetch(url,  { headers: {
+            'Accept': 'application/json'
+            }
+        });
+        if (!answer.ok) {
+            throw new Error(`Error HTTP: ${answer.status}`);
+        }
+
+        const dataJoke: DataJoke = await answer.json();
+
+        console.log(dataJoke);
+        return dataJoke;
+
+    } catch (error) {
+        console.error("Hubo un problema con la operación fetch:", error);
+    }
+}
+
