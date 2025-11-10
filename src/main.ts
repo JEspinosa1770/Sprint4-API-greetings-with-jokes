10372f26a85526582c51d81f226ea4d64e931dd4
JEspinosa1770 <jespinosa1770@gmail.com>
2025-11-10 12:32:30 +0100

    feat: add three buttons on index.html to vote. Add eventListeners on main.ts to detect new buttons. Define the default button.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 import { getJoke } from './joke.ts';
 import { printJoke } from './utils.ts';
 import { voteJoke } from './vote-joke.ts';
+import { voteSelection } from './utils.ts';
 
 const btnJoke: HTMLButtonElement | null = document.querySelector('.btn__next-joke');
 const btnVote: HTMLButtonElement | null = document.querySelector('.btn__vote-joke');
@@ -20,11 +21,29 @@ btnJoke?.addEventListener('click', () => {
   });
 });
 
-btnVote?.addEventListener('click', () => {
+btnVote?.addEventListener('click', (event) => {
+  event.preventDefault();
   if (actualJoke.length > 0) {
-    voteJoke(actualJoke);
-    console.log("Votando: ", actualJoke);
+    voteJoke(actualJoke, currentValue);
   } else {
     console.warn("No se puede votar. Esperando la carga del chiste.");
   }
 });
+
+const buttons = document.querySelectorAll('.btn__select') as NodeListOf<HTMLButtonElement>;
+let currentValue: number = 0; 
+let defaultActive: boolean = true;
+
+if (defaultActive) {
+  const defaultButton = document.querySelector('.btn__select[data-value="2"]') as HTMLButtonElement | null;
+
+  if (defaultButton) {
+      currentValue = voteSelection({ currentTarget: defaultButton }, buttons );
+      defaultActive = false;
+  }
+}
+
+buttons.forEach(button => {
+  button.addEventListener('click', (event: MouseEvent) => currentValue = voteSelection(event, buttons))
+});
+    
