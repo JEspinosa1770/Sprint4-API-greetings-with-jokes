1437d523aae3f36e0547bf7b3ea65465c0db983c
JEspinosa1770 <jespinosa1770@gmail.com>
2025-11-10 13:32:05 +0100

    fix: console.log deleted in joke.ts.

--- src/joke.ts
+++ src/joke.ts
@@ -13,7 +13,6 @@ export async function getJoke(url: string): Promise<DataJoke | undefined> {
 
         const dataJoke: DataJoke = await answer.json();
 
-        console.log(dataJoke);
         return dataJoke;
 
     } catch (error) {
