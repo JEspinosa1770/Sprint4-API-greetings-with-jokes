41832a2962573ac613e089af6349f0846aeeb50e
JEspinosa1770 <jespinosa1770@gmail.com>
2025-11-10 12:42:47 +0100

    feat: add voteSelection and findJoke functions in utils.ts, to show wich button has been selected and to find out if the joke has already been voted or not.

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import { reportJokes } from "./ddbb";
 
 export function printJoke(joke: string): void {
   document.querySelector<HTMLDivElement>('.joke')!.innerHTML = `
@@ -6,3 +7,18 @@ export function printJoke(joke: string): void {
     </div>
   `
 }
+
+export function voteSelection(event: MouseEvent | { currentTarget: HTMLButtonElement }, 
+  buttons: NodeListOf<HTMLButtonElement>): number {
+  const selectedButton = event.currentTarget as HTMLButtonElement; 
+  const value = parseInt(selectedButton.getAttribute('data-value') || '0');
+
+  buttons.forEach(btn => {
+      btn.classList.remove('selected');
+  });
+  selectedButton.classList.add('selected');
+
+  return value;
+}
+
+export function findJoke(joke: string): number { return reportJokes.findIndex(elem => elem.joke == joke) }
