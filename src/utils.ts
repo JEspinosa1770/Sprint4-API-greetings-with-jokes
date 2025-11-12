c5465e7c9e0c2dc615101b1f50ece68a7c8705b8
JEspinosa1770 <jespinosa1770@gmail.com>
2025-11-12 12:48:20 +0100

    feat: add normalizeJoke and API_URL functions.

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,32 @@
 import { reportJokes } from "./ddbb";
-import type { DataJoke } from "./interfaces";
+import type { DataJoke, DataWeather } from "./interfaces";
+
+export const amountSources: number = 3;
+export const API_URL = (): Object => {
+  const randomJoke: number = Math.floor(Math.random() * amountSources);
+
+  let url: string = "";
+  switch (randomJoke) {
+      case 0:
+          url = 'https://icanhazdadjoke.com/';
+          break;
+      case 1:
+          url = 'https://api.chucknorris.io/jokes/random';
+          break; 
+      case 2:
+          url = 'https://official-joke-api.appspot.com/jokes/random';
+          break;  
+      default:
+          throw new Error(`Problemas con la fuente de chistes`);
+          // break;
+  }
+  return [url, randomJoke.toString()];
+}
 
 export function normalizeJoke(answer: Response, dataJoke: any, randomJoke: number): DataJoke {
   let jokeCatched: string;
   let idCatched: string;
+  console.log(randomJoke)
   switch (randomJoke) {
       case 0:
           jokeCatched = dataJoke.joke;
@@ -58,6 +81,31 @@ export function haveVotedMessage(voted: boolean): void {
   voted ? textMessage.textContent = "Has votat" : textMessage.textContent = "";
 }
 
-export function printWeather (message: string): void {
-  document.getElementById("weather")!.textContent = message;
+export function printWeather (dataWeather: DataWeather): void {
+  let messageSky: string = "";
+  const code: number = dataWeather.current_weather.weathercode;
+
+  switch (true) {
+      case code === 0:
+          messageSky = "cel clar";
+          break;
+      case code === 1:
+          messageSky = "algúns núvols";
+          break;
+      case code === 2:
+          messageSky = "uns quants núvols";
+          break;
+      case code === 3:
+          messageSky = "molts núvols";
+          break;
+      case code > 60 && code < 70:
+          messageSky = "pluja";
+          break;
+      default:
+          messageSky = "temps indeterminat. Mira per la finestra."
+          break;
+  }
+  const messageToPrint: string = `Informació meteorològica: ${dataWeather.current_weather.temperature}° i hi ha ${messageSky}`;
+
+  document.getElementById("weather")!.textContent = messageToPrint;
 }
